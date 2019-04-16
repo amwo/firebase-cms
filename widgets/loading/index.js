5244e1b7b7d382efdac1818564ed7f466f30c998
amwo <nuude.ad@gmail.com>
2019-04-16 16:27:07 +0900

    Add auth router

--- widgets/loading/index.js
+++ widgets/loading/index.js
@@ -1,10 +1,20 @@
 import {Component} from 'react'
 import style from './style.css'
 import LOGO from '../../assets/logo.png'
+import {connect} from 'redux-zero/react';
+import fn from '../../states/fn';
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import Router from 'next/router';
 
+const mapToProps = ({ s , d}) => ({ s, d });
 class Loading extends Component {
     constructor(props) {
         super(props)
+    }
+
+    componentDidUpdate() {
+        this.props.s.login ? Router.push('/') : Router.push('/login')
     }
 
     render() {
@@ -19,4 +29,4 @@ class Loading extends Component {
     }
 }
 
-export default Loading
+export default connect(mapToProps, fn)(Loading)
